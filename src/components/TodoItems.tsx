bfafd2a01e0489159386b725fa23ccaa2b0fe0c2
thaodangJS_MACOS <thaohoangjs@MacBook-Air-cua-Thao.local>
2023-12-17 18:07:49 +0700

    reducer with ts

--- src/components/TodoItems.tsx
+++ src/components/TodoItems.tsx
@@ -1,4 +1,4 @@
-import React, { FormEvent, useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Todo } from "../model";
 import { AiFillEdit, AiFillDelete } from "react-icons/ai";
 import { MdDone } from "react-icons/md";
@@ -37,7 +37,7 @@ const TodoItems = ({ todo, todos, setTodos }: Props) => {
   const inputRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
-    inputRef.current?.focus();
+    inputRef.current?.focus(); //moi khi edit thay doi thi focus vao
   }, [edit]);
 
   return (
--- src/model.ts
+++ src/model.ts
@@ -3,3 +3,29 @@ export interface Todo {
   todo: string;
   isDone: boolean;
 }
+
+// type Actions =
+//   | { type: "add"; payload: string }
+//   | { type: "remove"; payload: number }
+//   | { type: "done"; payload: number };
+
+// import { useReducer } from "react";
+
+// const TodoReducer = (state: Todo[], action: Actions) => {
+//   switch (action.type) {
+//     case "add":
+//       return [
+//         ...state,
+//         { id: Date.now(), todo: action.payload, isDone: false },
+//       ];
+//     case "remove":
+//       return [...state, { id }];
+//     case "done":
+//     default:
+//       return state;
+//   }
+// };
+
+// const ReducerExample = () => {
+//   const [state, dispatch] = useReducer(TodoReducer, []);
+// };
