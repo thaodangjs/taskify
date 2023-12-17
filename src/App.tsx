f75a5d3a22f1a07a6010328d0b4b115e76eadf39
Thao Dang <thaodang.js@gmail.com>
2023-12-17 10:26:25 +0700

    added model file to define type

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
+import { useState } from "react";
 import "./App.css";
 import InputFeild from "./components/InputFeild";
 
 const App: React.FC = () => {
+  const [todo, setTodo] = useState<string | number>("");
   return (
     <div className="App">
       <span className="heading">Taskify</span>
-      <InputFeild />
+      <InputFeild todo={todo} setTodo={setTodo} />
     </div>
   );
 };
--- src/components/InputFeild.tsx
+++ src/components/InputFeild.tsx
@@ -1,12 +1,26 @@
 import React from "react";
 import "./styles.css";
-export default function InputFeild() {
+
+interface Props {
+  todo: string | number;
+  setTodo: React.Dispatch<React.SetStateAction<string | number>>;
+}
+
+const InputFeild: React.FC<Props> = ({ todo, setTodo }: Props) => {
   return (
     <form className="input">
-      <input type="input" placeholder="Enter a task" className="input__box" />
+      <input
+        type="input"
+        placeholder="Enter a task"
+        className="input__box"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+      />
       <button type="submit" className="input_submit">
         Go
       </button>
     </form>
   );
-}
+};
+
+export default InputFeild;
