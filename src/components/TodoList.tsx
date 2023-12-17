beeb9d9bed24469a88571e2f57f45ace0adcea20
thaodangJS_MACOS <thaohoangjs@MacBook-Air-cua-Thao.local>
2023-12-17 18:41:40 +0700

    finish styles active tasks & completed tasks

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -9,16 +9,30 @@ interface Props {
 
 const TodoList: React.FC<Props> = ({ todos, setTodos }: Props) => {
   return (
-    <ul className="todos">
-      {todos.map((todo) => (
-        <TodoItems
-          todo={todo}
-          key={todo.id}
-          todos={todos}
-          setTodos={setTodos}
-        />
-      ))}
-    </ul>
+    <div className="container">
+      <div className="todos">
+        <span className="todos__heading">Active tasks</span>
+        {todos.map((todo) => (
+          <TodoItems
+            todo={todo}
+            key={todo.id}
+            todos={todos}
+            setTodos={setTodos}
+          />
+        ))}
+      </div>
+      <div className="todos remove">
+        <span className="todos__heading">Completed tasks</span>
+        {todos.map((todo) => (
+          <TodoItems
+            todo={todo}
+            key={todo.id}
+            todos={todos}
+            setTodos={setTodos}
+          />
+        ))}
+      </div>
+    </div>
   );
 };
 
